e43c13476e68bbb23a9ca33ba81e9a9aae6ec4c7
vheymans <vheymans@student.42wolfsburg.de>
2022-05-31 11:21:08 -0700

    quick

--- module01/ex03/Weapon.hpp
+++ module01/ex03/Weapon.hpp
@@ -9,6 +9,11 @@ class Weapon
 	public :
 		Weapon ( void );
 		~Weapon ( void );
+
+	private :
+		std::string type;
+		&std::string	getType( void );
+		void setType( std:: string type );
 };
 
 #endif
