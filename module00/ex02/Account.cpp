cd53f230e85247f5fac60a603184c7237a4840f9
Vincent Heymans <belgishboy@gmail.com>
2022-06-01 10:39:13 +0200

    update

---
+++ module00/ex02/Account.cpp
@@ -0,0 +1,42 @@
+
+#include "Account.hpp"
+
+Account::Account(int initial_deposit)
+{
+
+}
+
+static int	Account::getNbAccounts( void )
+{
+
+}
+
+static int	Account::getTotalAmount( void )
+{
+
+}
+
+static int	Account::getNbDeposits( void )
+{
+
+}
+
+static int	Account::getNbWithdrawals( void )
+{
+
+}
+
+static void	Account::displayAccountsInfos( void )
+{
+
+}
+
+static void	Account::_displayTimestamp( void )
+{
+	
+}
+
+Account::~Account( void )
+{
+
+}
