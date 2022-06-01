d9a09930a5282f50f37c6a8f4e56e47b89c28b15
Vincent Heymans <belgishboy@gmail.com>
2022-06-01 16:26:17 +0200

    little housekeeping

--- module01/ex02/main.cpp
+++ module01/ex02/main.cpp
@@ -17,5 +17,26 @@ int main (void)
 	std::cout << std::endl << "stringPTR : " << *stringPTR << std::endl;
 	std::cout << std::endl << "stringREF : " << stringREF << std::endl;
 
+	s = "BYE";
+
+	std::cout << std::endl << "string : " << &s << std::endl;
+	std::cout << std::endl << "stringPTR : " << &stringPTR << std::endl;
+	std::cout << std::endl << "stringREF : " << &stringREF << std::endl;
+
+	std::cout << std::endl << "string : " << s << std::endl;
+	std::cout << std::endl << "stringPTR : " << *stringPTR << std::endl;
+	std::cout << std::endl << "stringREF : " << stringREF << std::endl;
+
+	std::string t = "MAMA";
+	stringPTR = &t;
+
+		std::cout << std::endl << "string : " << &s << std::endl;
+	std::cout << std::endl << "stringPTR : " << &stringPTR << std::endl;
+	std::cout << std::endl << "stringREF : " << &stringREF << std::endl;
+
+	std::cout << std::endl << "string : " << s << std::endl;
+	std::cout << std::endl << "stringPTR : " << *stringPTR << std::endl;
+	std::cout << std::endl << "stringREF : " << stringREF << std::endl;
+
 	return (0);
 }
