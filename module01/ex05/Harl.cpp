290f768e728f8a8e904696c68e1451baf0eb208e
Vincent Heymans <belgishboy@gmail.com>
2022-06-03 14:41:41 +0200

    setup of ex05

---
+++ module01/ex05/Harl.cpp
@@ -0,0 +1,15 @@
+
+#include "Harl.h"
+
+Harl::Harl ( void )
+{
+	std::cout << std::endl << "Harl  Created" << std::endl;
+	return ;
+}
+
+
+Harl::~Harl (void)
+{
+	std::cout << std::endl << "Harl Destroyed" << std::endl;
+	return ;
+}
---
+++ module01/ex05/Harl.hpp
@@ -0,0 +1,21 @@
+
+#ifndef HUMANA_HPP
+# define HUMANA_HPP
+
+# include "Harl.h"
+
+class Harl
+{
+	public :
+		Harl ( void );
+		~Harl ( void );
+		void complain ( std::string level );
+	
+	private :
+		void debug( void );
+		void info( void );
+		void warning( void );
+		void error(void);
+};
+
+#endif
