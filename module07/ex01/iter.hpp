b4b834462a59dfa0000175bd96e288635092b821
vheymans <vheymans@student.42wolfsburg.de>
2022-07-12 13:25:58 +0200

    ex01 done

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -21,3 +21,35 @@ T & min( T & x, T & y)
 {
 	return ( x < y ? x : y);
 }
+
+template <typename T>
+void plusPlus(T & x)
+{
+	x ++;
+
+	return ;
+}
+
+template <typename T, typename Y>
+void iter(T & add, size_t l, Y f)
+{
+	size_t _l = 0;
+	while (_l < l)
+	{
+		f(add[_l]);
+		_l ++;
+	}
+	return ;
+}
+
+template <typename T, typename Y>
+void iter(T & add, size_t l, void (swap)(Y &, Y &) )
+{
+	size_t _l = 0;
+	while (l > 1 && _l < l - 1)
+	{
+		swap(add[_l], add[_l + 1]);
+		_l += 2;
+	}
+	return ;
+}
--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -4,17 +4,25 @@
 
 int main( void )
 {
-	int a = 2;
-	int b = 3;
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
-	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	int		i[] = {1, 4, 6, 7, 22};
+	float	f[] = { 4.2, 5.3, 6.4 , 1.1 , 16.4};
+	char	c[] = { 'V', 'i', 'n', 'n', 'Y'};
+	std::cout << i[0] << " , " << i[1] << " , " << i[2] << " , " << i[3] << " , " << i[4] << std::endl;
+	std::cout << f[0] << " , " << f[1] << " , " << f[2] << " , " << f[3] << " , " << f[4] << std::endl;
+	std::cout << c[0] << " , " << c[1] << " , " << c[2] << " , " << c[3] << " , " << c[4] << std::endl << std::endl;
+
+	iter(i, 5, plusPlus<int>);
+	iter(f, 5, plusPlus<float>);
+	iter(c, 5, plusPlus<char>);
+	std::cout << i[0] << " , " << i[1] << " , " << i[2] << " , " << i[3] << " , " << i[4] << std::endl;
+	std::cout << f[0] << " , " << f[1] << " , " << f[2] << " , " << f[3] << " , " << f[4] << std::endl;
+	std::cout << c[0] << " , " << c[1] << " , " << c[2] << " , " << c[3] << " , " << c[4] << std::endl;
+
+	iter(i, 5, swap<int>);
+	iter(f, 5, swap<float>);
+	iter(c, 5, swap<char>);
+	std::cout << i[0] << " , " << i[1] << " , " << i[2] << " , " << i[3] << " , " << i[4] << std::endl;
+	std::cout << f[0] << " , " << f[1] << " , " << f[2] << " , " << f[3] << " , " << f[4] << std::endl;
+	std::cout << c[0] << " , " << c[1] << " , " << c[2] << " , " << c[3] << " , " << c[4] << std::endl;
 	return 0;
 }
